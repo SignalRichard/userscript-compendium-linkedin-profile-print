b9612fb20f34a1f9aa6c8b7552eeb0e7cb1d6f5c
Richard Bolhofer <20459042+SignalRichard@users.noreply.github.com>
2023-06-16 15:39:46 -0400

    refactor: typescript conversion

--- src/Compendium.LinkedInProfilePrint.user.ts
+++ src/Compendium.LinkedInProfilePrint.user.ts
@@ -16,43 +16,45 @@
 
 (async function() {
     'use strict';
-    var height = window.innerHeight / 2;
-    var scrollHeight = height;
+    let height = window.innerHeight / 2;
+    const scrollHeight = height;
     do {
         window.scrollTo(0, height);
         height += scrollHeight;
         await new Promise(r => setTimeout(r, 1000));
     } while(height < document.body.scrollHeight);
-    [...document.getElementsByClassName("scaffold-layout__content--has-aside")].forEach((item) => { item.parentElement.setAttribute("class", ""); item.setAttribute("class", ""); });
+    [...document.getElementsByClassName("scaffold-layout__content--has-aside")].forEach((item) => { item.parentElement?.setAttribute("class", ""); item.setAttribute("class", ""); });
     [...document.getElementsByClassName("scaffold-layout__aside"),
     ...document.getElementsByTagName("header"),
     ...document.getElementsByTagName("footer")].forEach((item) => { if(item) { item.remove(); } });
-    var iterator = 0;
+    let iterator = 0;
     [...document.querySelectorAll("section.artdeco-card h2.pvs-header__title>span[aria-hidden=true]")].forEach((item) => {
         iterator++;
-        switch (item.innerText) {
-            case "Highlights":
-            case "Featured":
-            case "Activity":
-            case "Licenses & certifications":
-            case "Skills":
-            case "Recommendations":
-            case "Courses":
-            case "Languages":
-            case "Organizations":
-            case "Causes":
-            case "Interests":
-            case "People also viewed":
-            case "People you may know":
-            case "You might like":
-                item.setAttribute("id", `tamper-${iterator}`)
-                document.querySelector(`#tamper-${iterator}`).closest(".artdeco-card").remove();
-                break;
-            default:
-                break;
+        if(item instanceof HTMLElement) {
+            switch (item.innerText) {
+                case "Highlights":
+                case "Featured":
+                case "Activity":
+                case "Licenses & certifications":
+                case "Skills":
+                case "Recommendations":
+                case "Courses":
+                case "Languages":
+                case "Organizations":
+                case "Causes":
+                case "Interests":
+                case "People also viewed":
+                case "People you may know":
+                case "You might like":
+                    item.setAttribute("id", `tamper-${iterator}`)
+                    document.querySelector(`#tamper-${iterator}`)?.closest(".artdeco-card")?.remove();
+                    break;
+                default:
+                    break;
+            }
         }
     });
-    [...document.getElementsByClassName("inline-show-more-text__button")].forEach((item) => { item.click(); });
+    [...document.getElementsByClassName("inline-show-more-text__button")].forEach((item) => { if(item instanceof HTMLElement) { item.click(); } });
     await new Promise(r => setTimeout(r, 1000));
     window.scrollTo(0, 0);
     window.print();
